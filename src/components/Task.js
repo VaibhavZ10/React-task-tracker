8b1b0a85d56a55b2b03290faf4fe1169d795f7ea
vaibhav <vaibhav.sindal@gmail.com>
2022-05-28 04:18:32 +0530

    adding

---
+++ src/components/Task.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaMixer } from "react-icons/fa";
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}
+        <FaMixer
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+          //   onClick={() => onToggle(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
